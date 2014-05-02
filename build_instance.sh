c0ed46e410b69212f3f692fc4d8a4fff68f7d12b
Kent Georgeson <kent@georgesons.net>
2014-05-02 13:30:12 -0400

    split to 2

---
+++ build_instance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+echo FONT=Lat2-Terminus16 > /etc/vconsole.conf
+ln -s /user/share/zoneinfo/US/Eastern /etc/localtime
+hwclock --systohc --localtime
+echo k2 > /etc/hostname
+systemctl enable dhcpcd.service
+pacman -S syslinux
+syslinux-intsall_update -i -a -m
+exit
+
+umount -R /mnt
+reboot
--- install_arch.sh
+++ install_arch.sh
@@ -19,21 +19,5 @@ mount /dev/sda2 /mnt/home
 pacstrap -i /mnt base
 genfstab -U -p /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash
-
-
-echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
-locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-export LANG=en_US.UTF-8
-echo FONT=Lat2-Terminus16 > /etc/vconsole.conf
-ln -s /user/share/zoneinfo/US/Eastern /etc/localtime
-hwclock --systohc --localtime
-echo k2 > /etc/hostname
-systemctl enable dhcpcd.service
-pacman -S grub
-grub-install --target=i386-pc --recheck /dev/sda1
-grub-mkconfig -o /boot/grub/grub.conf
-exit
-
 umount -R /mnt
 reboot
