e5783b287df25564a2803cb72667df7460481f17
Kent Georgeson <kent@georgesons.net>
2014-05-02 16:46:16 -0400

    another update

--- install_arch.sh
+++ install_arch.sh
@@ -16,11 +16,11 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda1 /mnt
 mkdir /mnt/home
 mount /dev/sda2 /mnt/home
-pacstrap -i /mnt base --noconfirm
+pacstrap -i /mnt base git vim --noconfirm
 genfstab -U -p /mnt >> /mnt/etc/fstab
 mkdir /mnt/home/root
 cp build_instance.sh /mnt/home/root/
 chmod 755 /mnt/home/root/build_instance.sh
 arch-chroot /mnt /bin/bash
-umount -R /mnt
-reboot
+# umount -R /mnt
+# reboot
