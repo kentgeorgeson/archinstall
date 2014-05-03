be1ca93c6806d7de37c65c96abd537bdcee94102
Kent Georgeson <kent@georgesons.net>
2014-05-03 15:43:34 +0000

    updated prior to aur

--- build_instance.sh
+++ build_instance.sh
@@ -14,12 +14,12 @@ grub-mkconfig -o /boot/grub/grub.cfg
 echo Set Root Password....
 passwd
 echo Adding User
-adduser -g users kent
+useradd -g users kent
 echo Set User Password...
 passwd kent
 mkdir /home/kent
 echo "kent ALL=(ALL) ALL" >> /etc/sudoers
-pacman -S ttf-dejavu ttf-ubuntu-font-family ttf-droid pacman-optimize reflector --noconfirm
+pacman -S ttf-dejavu ttf-ubuntu-font-family ttf-droid reflector --noconfirm
 cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkup
 reflector --verbose -l 15 -p http -sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy
