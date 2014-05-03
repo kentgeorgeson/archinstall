51a15109f375ad24e1a7e4619668d6de0423a6e5
Kent Georgeson <kent@georgesons.net>
2014-05-03 15:26:57 +0000

    additional prep

--- build_instance.sh
+++ build_instance.sh
@@ -9,5 +9,21 @@ hwclock --systohc --localtime
 echo k2 > /etc/hostname
 systemctl enable dhcpcd.service
 pacman -S grub --noconfirm
-grub-install --target=i386-pc --recheck /dev/sda
+grub-install --force --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
+echo Set Root Password....
+passwd
+echo Adding User
+adduser -g users kent
+echo Set User Password...
+passwd kent
+mkdir /home/kent
+echo "kent ALL=(ALL) ALL" >> /etc/sudoers
+pacman -S ttf-dejavu ttf-ubuntu-font-family ttf-droid pacman-optimize reflector --noconfirm
+cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkup
+reflector --verbose -l 15 -p http -sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syy
+
+echo "complete -cf sudo" >> /home/kent/.bashrc
+echo "complete -cf man" >> /home/kent/.bashrc
+# exit
--- install_arch.sh
+++ install_arch.sh
@@ -6,8 +6,8 @@ export LANG=en_US.UTF-8
 systemctl start dhcpcd.service
 sgdisk --zap-all /dev/sda
 sfdisk /dev/sda << EOF
-20,768,,*
-,,,
+1,768,L,*
+769,,L,
 ;
 ;
 EOF
@@ -16,11 +16,10 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda1 /mnt
 mkdir /mnt/home
 mount /dev/sda2 /mnt/home
-pacstrap -i /mnt base git vim --noconfirm
+pacstrap -i /mnt base sudo git vim --noconfirm
 genfstab -U -p /mnt >> /mnt/etc/fstab
-mkdir /mnt/home/root
-cp build_instance.sh /mnt/home/root/
-chmod 755 /mnt/home/root/build_instance.sh
+cp build_instance.sh /mnt/root/
+chmod 755 /mnt/root/build_instance.sh
 arch-chroot /mnt /bin/bash
-# umount -R /mnt
-# reboot
+umount -R /mnt
+reboot
